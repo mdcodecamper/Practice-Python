d7d107bcbb63a6a67fa85f42c2c03fb0d42a4104
codecamper <codecamper@outlook.com>
2018-06-10 23:20:24 -0400

    added new file

---
+++ Character_Input.py
@@ -0,0 +1,10 @@
+
+name = input("Please Enter Your Name: ")
+age = int(input("Please Enter Your Age: "))
+
+currentYear = 2018
+tooBeOld = str((currentYear - age) + 100)
+
+print(name + ", You are going to 100 years old in " + tooBeOld + ".")
+
+
